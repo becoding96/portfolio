fd5e36cc4e6df19f19a6fc8cf923c49b23918b13
becoding96 <becoding96@gmail.com>
2023-09-04 15:22:31 +0900

    feat: 네브바 UI 구성

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from 'next/link'
+import styles from "./Navbar.module.scss";
+
+function Navbar() {
+  return (
+    <div>
+      <div id={styles.navbar}>
+        <div className={styles.normal} style={{ justifyContent: "flex-start" }}>
+          <Link href="" style={{ marginRight: "20px" }}>
+            ABOUT
+          </Link>
+          <Link href="">PROJECTS</Link>
+        </div>
+        <div style={{ justifyContent: "center" }}>
+          <Link id={styles.center} href="">
+            <span style={{ fontWeight: "bold", fontSize: "20px !important" }}>BECO</span>ding
+          </Link>
+        </div>
+        <div className={styles.normal} style={{ justifyContent: "flex-end" }}>
+          <Link href="">CONTACT</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
