8c25953febe8f7eda9da4eafb8b03249a2473180
becoding96 <becoding96@gmail.com>
2023-09-04 15:22:17 +0900

    feat: 배경 채우기

---
+++ src/components/FillBackground/FillBackground.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./FillBackground.module.scss";
+
+function FillBackground() {
+  return <div id={styles.fill} />;
+}
+
+export default FillBackground;
