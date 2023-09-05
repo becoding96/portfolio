ac2ce7fb5bcc4b5e110380bf0bcd1b206a67e117
becoding96 <becoding96@gmail.com>
2023-09-05 09:41:09 +0900

    fix: ReferenceError: window is not defined 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-import Cursor from "@/components/Cursor/Cursor";
+import dynamic from "next/dynamic";
 import FillBackground from "@/components/FillBackground/FillBackground";
 import Navbar from "@/components/Navbar/Navbar";
 import Main from "@/containers/Main/Main";
 
 export default function Home() {
+  const Cursor = dynamic(() => import("@/components/Cursor/Cursor"), {
+    ssr: false,
+  });
+
   return (
     <>
       <Cursor />
       <FillBackground />
       <Navbar />
       <Main />
-      <div style={{ height: "1000px" }}></div>
     </>
   );
 }
