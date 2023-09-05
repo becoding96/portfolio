139ce1c361382f64f2ca8f06c7ccbb23718660b8
becoding96 <becoding96@gmail.com>
2023-09-06 04:00:38 +0900

    style: css 일부 수정

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Link from 'next/link'
+import Link from "next/link";
 import styles from "./Navbar.module.scss";
 
 function Navbar() {
   return (
-    <div>
-      <div id={styles.navbar}>
+    <div id={styles.navbar}>
+      <div id={styles["navbar-inner"]}>
         <div className={styles.normal} style={{ justifyContent: "flex-start" }}>
           <Link href="" style={{ marginRight: "20px" }}>
             ABOUT
@@ -14,7 +14,10 @@ function Navbar() {
         </div>
         <div style={{ justifyContent: "center" }}>
           <Link id={styles.center} href="">
-            <span style={{ fontWeight: "bold", fontSize: "20px !important" }}>BECO</span>ding
+            <span style={{ fontWeight: "bold", fontSize: "20px !important" }}>
+              BECO
+            </span>
+            ding
           </Link>
         </div>
         <div className={styles.normal} style={{ justifyContent: "flex-end" }}>
