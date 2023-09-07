ab1bd79aa0b3fc301d9f356b706df8b305f1feaf
becoding96 <becoding96@gmail.com>
2023-09-07 13:07:17 +0900

    feat: About 컨테이너 세팅

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./About.module.scss";
+import ContainerTitle from "@/components/ContainerTitle/ContainerTitle";
 
 function About() {
-  return <div id={styles.container}></div>;
+  return (
+    <div id={styles.container}>
+      <ContainerTitle title="ABOUT" nth={1} />
+    </div>
+  );
 }
 
 export default About;
