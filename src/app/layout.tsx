1e73dfb46f202df0594c6fa88da891ec1c5285db
becoding96 <becoding96@gmail.com>
2023-09-11 21:24:16 +0900

    feat: 파비콘 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "백준봉 | 프론트엔드 개발자 포트폴리오",
   description: "프론트엔드 개발자 백준봉의 포트폴리오 사이트입니다.",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
