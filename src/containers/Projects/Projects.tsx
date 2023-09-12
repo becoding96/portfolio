f563c9fe62547e959ff7a7cfc5dc0da0889cd420
becoding96 <becoding96@gmail.com>
2023-09-13 00:45:26 +0900

    style: 전반적인 css 수정

--- src/containers/Projects/Projects.tsx
+++ src/containers/Projects/Projects.tsx
@@ -12,7 +12,7 @@ function Projects({
 }: {
   projectsRef: React.MutableRefObject<HTMLDivElement | null>;
 }) {
-  const multiplier: number = 1.5;
+  const multiplier: number = 1.8;
   const [showProjects, setShowProjects] = useState<boolean>(false);
 
   const handleScroll = () => {
@@ -38,7 +38,7 @@ function Projects({
           projectTitle="레포몬"
           projectTitleEng="Repomon"
           projectSummary="Github 레포지토리 분석 및 시각화 서비스"
-          projectFrom="삼성 청년 SW 아카데미 자율 프로젝트"
+          projectFrom="삼성 청년 SW 아카데미 자율 프로젝트 (6인)"
           projectAward="우수상 (2등)"
           projectImageLength={10}
           projectPeriod="2023.04.10 ~ 2023.05.19"
@@ -58,7 +58,7 @@ function Projects({
             "성장한 레포몬은 다른 레포몬과 전투할 수 있고, 카드 형태로 추출하여 README에 활용할 수 있습니다.",
           ]}
           projectRole={[
-            "6명 중 프론트엔드 팀장, 디자인을 담당했습니다.",
+            "프론트엔드 팀장, 디자인을 담당했습니다.",
             "소셜 로그인, 메인 페이지, 레포지토리 분석 페이지, 랭킹페이지, 크롬 익스텐션을 제작했습니다.",
           ]}
         />
@@ -66,7 +66,7 @@ function Projects({
           projectTitle="아이유정"
           projectTitleEng="IUJ"
           projectSummary="학부모 부동산 추천 서비스"
-          projectFrom="삼성 청년 SW 아카데미 특화 프로젝트"
+          projectFrom="삼성 청년 SW 아카데미 특화 프로젝트 (6인)"
           projectImageLength={4}
           projectPeriod="2023.02.20 ~ 2023.04.07"
           projectTechStack={[
@@ -83,7 +83,7 @@ function Projects({
             "원하는 주변 시설을 고려한 맞춤 추천 점수가 동반된 부동산 매물 리스트를 제공합니다.",
           ]}
           projectRole={[
-            "6명 중 프론트엔드 팀장, 디자인을 담당했습니다.",
+            "프론트엔드 팀장, 디자인을 담당했습니다.",
             "소셜 로그인, 메인 페이지, 지도 페이지, 관심 매물 페이지를 제작했습니다.",
           ]}
         />
@@ -91,7 +91,7 @@ function Projects({
           projectTitle="펀티어"
           projectTitleEng="Funteer"
           projectSummary="봉사 크라우드 펀딩 서비스"
-          projectFrom="삼성 청년 SW 아카데미 공통 프로젝트"
+          projectFrom="삼성 청년 SW 아카데미 공통 프로젝트 (6인)"
           projectImageLength={1}
           projectPeriod="2023.01.03 ~ 2023.02.17"
           projectTechStack={[
@@ -108,7 +108,7 @@ function Projects({
             "펀딩 성공 이후 봉사 활동은 라이브 방송으로 중계되어야 하며, 개인 회원들이 방송 도중 후원할 수 있습니다.",
           ]}
           projectRole={[
-            "6명 중 프론트엔드 팀원, 디자인을 담당했습니다.",
+            "프론트엔드 팀원, 디자인을 담당했습니다.",
             "회원가입(개인, 단체), 관리자 페이지, 충전 및 환불(아임포트), 프로필 페이지, 고객센터, 서비스 소개를 제작했습니다.",
           ]}
         />
