7347721a6c56c495a14a6fda94750f9c19f37dc3
becoding96 <becoding96@gmail.com>
2023-09-12 22:11:55 +0900

    feat: 프로젝트 상세 설명 추가

--- src/components/Modal/ProjectModal.tsx
+++ src/components/Modal/ProjectModal.tsx
@@ -154,14 +154,66 @@ function ProjectModal({
               </div>
               <div className={styles.hr} />
               <div className={styles["project-detail"]}>
-                <p>프로젝트 소개</p>
+                <p className={styles.subheading}>프로젝트 소개</p>
                 {projectDes.map((desLi, index) => (
                   <li key={index}>{desLi}</li>
                 ))}
-                <p>담당 역할</p>
+                <p className={styles.subheading}>담당 역할</p>
                 {projectRole.map((role, index) => (
                   <li key={index}>{role}</li>
                 ))}
+                {projectTitleEng !== "Funteer" && (
+                  <p className={styles.subheading}>페이지 상세 설명</p>
+                )}
+                {projectTitleEng === "Repomon" && (
+                  <div className={styles.detail}>
+                    <p>
+                      레포지토리 분석 페이지는 프로젝트 갱신, 프로젝트의 성장
+                      곡선(경험치로 표현), 컨벤션 준수율, 개인의 프로젝트
+                      기여도,
+                    </p>
+                    <p>
+                      레포몬 배틀에 관한 정보(레이팅, 능력치, 전적, 순위 ...)
+                      등의 기능이 포함되어 있습니다.
+                    </p>
+                    <br />
+                    <p>
+                      크롬 익스텐션은 {"'레포몬 배틀'"}이라는 일부 기능을 다른
+                      작업을 하면서 함께 즐길 수 있도록 크롬 웹 스토어에
+                    </p>
+                    <p> 등록했습니다.</p>
+                  </div>
+                )}
+                {projectTitleEng === "IUJ" && (
+                  <div className={styles.detail}>
+                    <p>
+                      지도의 매물은 점수가 적혀있는 집 모양의 마커로
+                      표현했습니다.
+                    </p>
+                    <br />
+                    <p>
+                      원하는 주변 시설을 설정하고, 저장할 수 있습니다. 저장한
+                      주변 시설은 언제든 불러와 적용할 수 있습니다.
+                    </p>
+                    <p>
+                      주변 시설을 설정하면 매물의 추천 점수에 해당 시설들이
+                      반영됩니다.
+                    </p>
+                    <br />
+                    <p>
+                      좌측 탭의 매물 목록에 점수에 대한 근거를 볼 수 있습니다.
+                    </p>
+                    <p>
+                      즉, 주변 시설로 초등학교, 지하철, 병원, 치안, 도서관을
+                      선택했다면 각각에 대한 점수를 볼 수 있습니다.
+                    </p>
+                    <br />
+                    <p>
+                      필터 기능이 포함되어 있습니다. 부동산 매물 종류, 평수,
+                      층수, 거래 종류를 선택할 수 있습니다.
+                    </p>
+                  </div>
+                )}
               </div>
             </div>
           </div>,
