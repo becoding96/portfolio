825b2b5743cda04afd23382cdfbb06cbf8630e8b
becoding96 <becoding96@gmail.com>
2023-09-13 14:14:23 +0900

    fix: 메인 소개글 show 로직 수정

--- src/components/Introduce/Introduce.tsx
+++ src/components/Introduce/Introduce.tsx
@@ -30,52 +30,57 @@ function Introduce() {
 
   useHandleScroll(handleScroll);
 
-  if (!showIntroduce) {
-    return null;
-  }
-
   return (
-    <div id={styles.lines}>
-      {lineLength >= 1 && (
-        <h1 style={{ position: "relative", marginRight: "20px" }}>
-          안녕하세요{" "}
-          {lineLength >= 2 && (
-            <span id={styles.ex} className={styles.highlight}>
-              !
-            </span>
+    <>
+      {showIntroduce && (
+        <div id={styles.lines}>
+          {lineLength >= 1 && (
+            <h1 style={{ position: "relative", marginRight: "20px" }}>
+              안녕하세요{" "}
+              {lineLength >= 2 && (
+                <span id={styles.ex} className={styles.highlight}>
+                  !
+                </span>
+              )}
+            </h1>
           )}
-        </h1>
+          {lineLength >= 3 && (
+            <h1>
+              <span className={styles.highlight}>
+                <ReactTypingEffect
+                  text={[
+                    "프론트엔드",
+                    "성장하는",
+                    "유저 친화적인",
+                    "빨리 배우는",
+                  ]}
+                  speed={200}
+                  eraseSpeed={100}
+                  eraseDelay={1500}
+                  typingDelay={1000}
+                />
+              </span>{" "}
+              개발자
+            </h1>
+          )}
+          {lineLength >= 4 && (
+            <h1>
+              <span>백준봉</span>입니다.
+            </h1>
+          )}
+          {lineLength >= 5 && (
+            <button
+              id={styles["scroll-btn"]}
+              onClick={() => {
+                scrollTo({ top: window.innerHeight, behavior: "smooth" });
+              }}
+            >
+              <span>View my work</span> <FiArrowDown id={styles.arrow} />
+            </button>
+          )}
+        </div>
       )}
-      {lineLength >= 3 && (
-        <h1>
-          <span className={styles.highlight}>
-            <ReactTypingEffect
-              text={["프론트엔드", "성장하는", "유저 친화적인", "빨리 배우는"]}
-              speed={200}
-              eraseSpeed={100}
-              eraseDelay={1500}
-              typingDelay={1000}
-            />
-          </span>{" "}
-          개발자
-        </h1>
-      )}
-      {lineLength >= 4 && (
-        <h1>
-          <span>백준봉</span>입니다.
-        </h1>
-      )}
-      {lineLength >= 5 && (
-        <button
-          id={styles["scroll-btn"]}
-          onClick={() => {
-            scrollTo({ top: window.innerHeight, behavior: "smooth" });
-          }}
-        >
-          <span>View my work</span> <FiArrowDown id={styles.arrow} />
-        </button>
-      )}
-    </div>
+    </>
   );
 }
 
