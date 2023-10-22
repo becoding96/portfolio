03e086bd6e70a575227b7fde2f84dfd9da5e08e7
becoding96 <becoding96@gmail.com>
2023-10-23 04:40:43 +0900

    feat: 스크롤에 따른 메인 화면 컨텐츠 opacity 조절

--- src/components/Circle/Circle.tsx
+++ src/components/Circle/Circle.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
 import styles from "./Circle.module.scss";
+import useHandleScroll from "@/hooks/useHandleScroll";
 
 function Circle() {
+  const circleRef = useRef<HTMLDivElement>(null);
+
+  const handleScroll = () => {
+    if (!circleRef || !circleRef.current) {
+      return;
+    }
+
+    if (window.scrollY > window.innerHeight / 3) {
+      circleRef.current.style.opacity = "0";
+    } else {
+      circleRef.current.style.opacity =
+        window.innerWidth <= 700 ? "0.25" : "0.9";
+    }
+  };
+
+  useHandleScroll(handleScroll);
+
   return (
-    <div className={styles.circles} id={styles.circles}>
-      <div className={styles.circle}></div>
-      <div className={styles.circle}></div>
-      <div className={styles.circle}></div>
-      <div className={styles.circle}></div>
+    <div className={styles.circles} id={styles.circles} ref={circleRef}>
+      <div className={styles.circle} />
+      <div className={styles.circle} />
+      <div className={styles.circle} />
+      <div className={styles.circle} />
     </div>
   );
 }
--- src/components/Introduce/Introduce.tsx
+++ src/components/Introduce/Introduce.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "./Introduce.module.scss";
 import ReactTypingEffect from "react-typing-effect";
 import { FiArrowDown } from "react-icons/fi";
@@ -8,7 +8,7 @@ import useHandleScroll from "@/hooks/useHandleScroll";
 
 function Introduce() {
   const [lineLength, setLineLength] = useState<number>(0);
-  const [showIntroduce, setShowIntroduce] = useState<boolean>(true);
+  const introduceRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -21,66 +21,61 @@ function Introduce() {
   }, [lineLength]);
 
   const handleScroll = () => {
-    if (window.scrollY >= window.innerHeight * 2) {
-      setShowIntroduce(false);
+    if (!introduceRef || !introduceRef.current) {
+      return;
+    }
+
+    if (window.scrollY > window.innerHeight / 3) {
+      introduceRef.current.style.opacity = "0";
     } else {
-      setShowIntroduce(true);
+      introduceRef.current.style.opacity = "1";
     }
   };
 
   useHandleScroll(handleScroll);
 
   return (
-    <>
-      {showIntroduce && (
-        <div id={styles.lines}>
-          {lineLength >= 1 && (
-            <h1 style={{ position: "relative", marginRight: "20px" }}>
-              안녕하세요{" "}
-              {lineLength >= 2 && (
-                <span id={styles.ex} className={styles.highlight}>
-                  !
-                </span>
-              )}
-            </h1>
+    <div id={styles.lines} ref={introduceRef}>
+      {lineLength >= 1 && (
+        <h1 style={{ position: "relative", marginRight: "20px" }}>
+          안녕하세요{" "}
+          {lineLength >= 2 && (
+            <span id={styles.ex} className={styles.highlight}>
+              !
+            </span>
           )}
-          {lineLength >= 3 && (
-            <h1>
-              <span className={styles.highlight}>
-                <ReactTypingEffect
-                  text={[
-                    "프론트엔드",
-                    "성장하는",
-                    "유저 친화적인",
-                    "빨리 배우는",
-                  ]}
-                  speed={200}
-                  eraseSpeed={100}
-                  eraseDelay={1500}
-                  typingDelay={1000}
-                />
-              </span>{" "}
-              개발자
-            </h1>
-          )}
-          {lineLength >= 4 && (
-            <h1>
-              <span>백준봉</span>입니다.
-            </h1>
-          )}
-          {lineLength >= 5 && (
-            <button
-              id={styles["scroll-btn"]}
-              onClick={() => {
-                scrollTo({ top: window.innerHeight, behavior: "smooth" });
-              }}
-            >
-              <span>View my work</span> <FiArrowDown id={styles.arrow} />
-            </button>
-          )}
-        </div>
+        </h1>
       )}
-    </>
+      {lineLength >= 3 && (
+        <h1>
+          <span className={styles.highlight}>
+            <ReactTypingEffect
+              text={["프론트엔드", "성장하는", "유저 친화적인", "빨리 배우는"]}
+              speed={200}
+              eraseSpeed={100}
+              eraseDelay={1500}
+              typingDelay={1000}
+            />
+          </span>{" "}
+          개발자
+        </h1>
+      )}
+      {lineLength >= 4 && (
+        <h1>
+          <span>백준봉</span>입니다.
+        </h1>
+      )}
+      {lineLength >= 5 && (
+        <button
+          id={styles["scroll-btn"]}
+          onClick={() => {
+            scrollTo({ top: window.innerHeight, behavior: "smooth" });
+          }}
+        >
+          <span>View my work</span> <FiArrowDown id={styles.arrow} />
+        </button>
+      )}
+    </div>
   );
 }
 
