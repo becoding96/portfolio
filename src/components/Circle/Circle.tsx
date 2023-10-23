6d605dd8bba3a18674fb66cd8ddbea8fd5fb6ed5
becoding96 <becoding96@gmail.com>
2023-10-23 11:41:24 +0900

    style: requestAnimationFrame 추가

--- src/components/Circle/Circle.tsx
+++ src/components/Circle/Circle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./Circle.module.scss";
 import useHandleScroll from "@/hooks/useHandleScroll";
 
@@ -8,24 +8,26 @@ function Circle() {
   const circleRef = useRef<HTMLDivElement>(null);
 
   const handleScroll = () => {
-    if (!circleRef || !circleRef.current) {
-      return;
-    }
+    requestAnimationFrame(() => {
+      if (!circleRef || !circleRef.current) {
+        return;
+      }
 
-    const scrollY = window.scrollY;
-    let size;
-    const opacity = 1 - scrollY / (window.innerHeight / 3);
+      const scrollY = window.scrollY;
+      let size;
+      const opacity = 1 - scrollY / (window.innerHeight / 3);
 
-    if (window.innerWidth <= 700) {
-      size = Math.min(80 + (scrollY / (window.innerHeight / 3)) * 20, 100);
-      circleRef.current.style.opacity = `${Math.min(0.25, opacity)}`;
-    } else {
-      size = Math.min(50 + (scrollY / (window.innerHeight / 3)) * 30, 80);
-      circleRef.current.style.opacity = `${Math.min(0.9, opacity)}`;
-    }
+      if (window.innerWidth <= 700) {
+        size = Math.min(80 + (scrollY / (window.innerHeight / 3)) * 20, 100);
+        circleRef.current.style.opacity = `${Math.min(0.25, opacity)}`;
+      } else {
+        size = Math.min(50 + (scrollY / (window.innerHeight / 3)) * 30, 80);
+        circleRef.current.style.opacity = `${Math.min(0.9, opacity)}`;
+      }
 
-    circleRef.current.style.width = `${size}vmin`;
-    circleRef.current.style.height = `${size}vmin`;
+      circleRef.current.style.width = `${size}vmin`;
+      circleRef.current.style.height = `${size}vmin`;
+    });
   };
 
   useHandleScroll(handleScroll);
